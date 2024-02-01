bb89a402d8e29da77ee70ec6d190ce25c09ba9fd
vignesh <vvignesh2913@gmail.com>
2024-02-01 17:10:36 +0530

    vig

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser');
 const path = require('path');
-
 const app = express();
 const port = 3000;
 
@@ -20,7 +19,7 @@ db.run(`
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.use(express.static('public'));
 // Serve HTML form for signup
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "public",'index.html'));
